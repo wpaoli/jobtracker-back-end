05619fbb62447d18b801186725ceeb5c43ec7a79
wpaoli <wpaoli@gmail.com>
2023-11-22 09:40:22 -0800

    Adjusted case of fields to match renaming in MySQL

--- jobs-routes.js
+++ jobs-routes.js
@@ -4,21 +4,7 @@ const db = require('./db');
 const cors = require('cors') 
 router.use(express.json());
 
-
-const corsOptions = {
-    // origin: '*',
-    // methods: '*',
-    // credentials: true,
-    // optionsSuccessStatus: 204,
-    // exposedHeaders: 'Content-Length'
-};
-
 router.use(cors());
-
-// router.get('/', (req, res) => {
-    
-//     res.send('Hello Worlds!')
-// });
 
 router.get('/jobs', (req, res) => {
     const sql = 'SELECT * FROM Jobs';
@@ -30,9 +16,9 @@ router.get('/jobs', (req, res) => {
 
 router.post('/job', (req, res) => {
     
-    const { JobTitle, Company, JobPosting, DateApplied, Notes } = req.body;
-    const sql = 'INSERT INTO Jobs (JobTitle, Company, JobPosting, DateApplied, Notes) VALUES (?, ?, ?, ?, ?)';
-    db.query(sql, [JobTitle, Company, JobPosting, DateApplied, Notes], (err, result) => {
+    const { job_title, company, job_posting, date_applied, notes } = req.body;
+    const sql = 'INSERT INTO Jobs (job_title, company, job_posting, date_applied, notes) VALUES (?, ?, ?, ?, ?)';
+    db.query(sql, [job_title, company, job_posting, date_applied, notes], (err, result) => {
         if (err) throw err;
         res.status(201).send('Job added');
     });
